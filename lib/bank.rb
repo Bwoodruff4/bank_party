99438617dcccdec488dec5d0487867587a1c63d0
Bwoodruff4 <blakewoodruff4@gmail.com>
2020-04-07 12:30:31 -0600

    Done except for last two methods.

--- lib/bank.rb
+++ lib/bank.rb
@@ -13,6 +13,30 @@ class Bank
         @@all
     end
 
+    def accounts
+        Account.all.select{|account| account.bank == self}
+    end
+
+    def customers
+        accounts.map{|account| account.customer }
+    end
+
+    def new_account(number, balance, customer)
+        Account.new(number, balance, self, customer)
+    end
+
+    def oldest_account
+        accounts.min_by(&:open_year)
+    end
+
+    def self.most_customers
+        all.max_by{|bank| bank.customers.length}
+    end
+
+    
+
+
+
     
 end 
 
--- lib/customer.rb
+++ lib/customer.rb
@@ -14,5 +14,26 @@ class Customer
         @@all
     end
 
+    def accounts
+        Account.all.select{|account| account.customer == self }
+    end
+
+    def banks
+        accounts.map{|account| account.bank.name }.uniq
+    end
+
+    def new_account(number, balance, bank)
+        Account.new(number, balance, bank, self)
+    end
+
+    def self.average_number_of_accounts
+        account_total=Account.all.length
+        account_total/all.length
+    end
+
+
+
+
+
    
 end
